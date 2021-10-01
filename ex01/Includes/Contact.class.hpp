1802e7aa35044c352f8d458b229541996407991e
Michael lucas Rosario kling <mrosario@c1r4s2.42madrid.com>
2021-10-01 13:49:13 +0200

    Added Makefile to ex01

---
+++ ex01/Includes/Contact.class.hpp
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Contact.class.hpp                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mrosario <mrosario@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/01 09:25:17 by mrosario          #+#    #+#             */
+/*   Updated: 2021/10/01 12:24:10 by mrosario         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CONTACT_CLASS_H
+# define CONTACT_CLASS_H
+
+#include <iostream>
+
+class	Contact {
+	public:
+
+	// std::string					first_name;
+	// std::string					last_name;
+	// std::string					nickname;
+	// std::string					phone_number;
+	// std::string					darkest_secret;
+	std::string					input_data[5];
+	static const std::string	input_msg[5];
+
+	
+
+	//me vale con los que vienen por defecto :p
+	//Contact(void);
+	// ~Contact(void);
+};
+
+#endif
