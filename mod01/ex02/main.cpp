e873dd443631903078097ee43906a5c502f408a2
Michael lucas Rosario kling <mrosario@c2r6s3.42madrid.com>
2021-10-06 23:56:57 +0200

    Makefile changes

--- mod01/ex02/main.cpp
+++ mod01/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: mrosario <mrosario@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/05 18:50:02 by mrosario          #+#    #+#             */
-/*   Updated: 2021/10/05 19:19:00 by mrosario         ###   ########.fr       */
+/*   Updated: 2021/10/06 23:49:13 by mrosario         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,9 +18,9 @@ int	main(void)
 	std::string	*brain_ptr = &brain_str;
 	std::string &brain_ref = brain_str;
 
-	std::cout << "String Address: " << &brain_str << "|" << std::endl
-			<< "String Pointer: " << brain_ptr << "|" << std::endl
-			<< "String Reference: " << &brain_ref << "|" << std::endl
+	std::cout << "String Address: " << &brain_str << std::endl
+			<< "String Pointer: " << brain_ptr << std::endl
+			<< "String Reference: " << &brain_ref << std::endl
 			<< "String via ptr: " << *brain_ptr << std::endl
 			<< "String via ref: " << brain_ref << std::endl;
 	
