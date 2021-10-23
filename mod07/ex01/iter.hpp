735f6371065bae9d8d44d810d5000badb9ff5698
Michael lucas Rosario kling <mrosario@c2r6s2.42madrid.com>
2021-10-24 00:08:57 +0200

    OK, that's exercise 00

--- mod07/ex01/iter.hpp
+++ mod07/ex01/iter.hpp
@@ -6,19 +6,39 @@
 /*   By: mrosario <mrosario@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/23 22:55:32 by mrosario          #+#    #+#             */
-/*   Updated: 2021/10/23 23:14:15 by mrosario         ###   ########.fr       */
+/*   Updated: 2021/10/24 00:07:58 by mrosario         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef ITER_H
 # define ITER_H
 
-template<typename ARR_PTR_T, typename ARR_LEN_T/*, typename FUN_T*/>
-void	iter(ARR_PTR_T arr_ptr, ARR_LEN_T arr_len/*, FUN_T fun*/)
+#define GRN "\e[1;32m"
+#define RST "\e[0m"
+
+template<typename ARR_PTR_T, typename ARR_LEN_T, typename FUN_T>
+void	iter(ARR_PTR_T arr_ptr, ARR_LEN_T const arr_len, FUN_T const fun)
 {
 	for (ARR_LEN_T i = 0; i < arr_len; ++i)
-		std::cout << arr_ptr[i];
+		std::cout << " " << arr_ptr[i];
 	std::cout << std::endl;
+	for (ARR_LEN_T i = 0; i < arr_len; ++i)
+		arr_ptr[i] = fun(arr_ptr[i]);
+	for (ARR_LEN_T i = 0; i < arr_len; ++i)
+		std::cout << " " << arr_ptr[i];
+	std::cout << std::endl;
+}
+
+template<typename T>
+T	funky_func(T a)
+{
+	return  (++a);
+}
+
+template<typename T>
+T	halve(T a)
+{
+	return (a / 2);
 }
 
 #endif
--- mod07/ex01/main.cpp
+++ mod07/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: mrosario <mrosario@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/23 23:09:45 by mrosario          #+#    #+#             */
-/*   Updated: 2021/10/23 23:13:23 by mrosario         ###   ########.fr       */
+/*   Updated: 2021/10/24 00:08:08 by mrosario         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,12 +15,24 @@
 
 int	main(void)
 {
-	char	chr_array[] = {"Hello World"};
+	char	chr_array[] = {"abcdefghijklmn"};
 	int		int_array[2] = {4,2};
+	float	float_array[2] = {4.42, 100.1};
 	
-	iter(chr_array, std::strlen(chr_array));
+	std::cout << "FUNKY FUNC" << std::endl;
+	iter(chr_array, std::strlen(chr_array), funky_func<char>);
 	std::cout << std::endl;
-	iter(int_array, 2);
+	iter(int_array, 2, funky_func<int>);
+	std::cout << std::endl;
+	iter(float_array, 2, funky_func<float>);
+	std::cout << std::endl;
+
+	std::cout << "HALVE" << std::endl;
+	iter(chr_array, std::strlen(chr_array), halve<char>);
+	std::cout << std::endl;
+	iter(int_array, 2, halve<int>);
+	std::cout << std::endl;
+	iter(float_array, 2, halve<float>);
 
 	return (0);
 }
