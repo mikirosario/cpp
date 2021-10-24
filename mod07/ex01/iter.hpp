d3978b60000d0e688fbf1485309001bf69368858
Michael lucas Rosario kling <mrosario@c2r6s2.42madrid.com>
2021-10-24 21:53:41 +0200

    Fixed ex01

--- mod07/ex01/iter.hpp
+++ mod07/ex01/iter.hpp
@@ -6,7 +6,7 @@
 /*   By: mrosario <mrosario@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/23 22:55:32 by mrosario          #+#    #+#             */
-/*   Updated: 2021/10/24 00:07:58 by mrosario         ###   ########.fr       */
+/*   Updated: 2021/10/24 21:51:13 by mrosario         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,29 +16,53 @@
 #define GRN "\e[1;32m"
 #define RST "\e[0m"
 
-template<typename ARR_PTR_T, typename ARR_LEN_T, typename FUN_T>
-void	iter(ARR_PTR_T arr_ptr, ARR_LEN_T const arr_len, FUN_T const fun)
+/*
+** En la primera prueba supuse que la función pasada a iter devolvería una
+** referencia a la variable tipo T, pero esperan que sea void.
+**
+** También me lié declarando el puntero al array (se me olvidó poner el
+** asterisco, vamos xD), y creé un iter muy sui géneris que no solo aplicaba
+** la función pasada sino que además hacía pruebas dentro para ahorrarme líneas
+** en el main. O sea, un bicho que solo rulaba con mis funciones.
+**
+** Tampoco en el subject especificaban cómo tiene que ser la función que se
+** pasa a iter, así que lo hice a la ventura pensando que era libre, y me
+** devolvía una referencia de tipo T. Pues no, es de tipo void. :P
+**
+** Total, no casaba con la función print de la prueba de la escuela, y
+** no supe arreglarlo durante la evaluación porque todavía me lío con los
+** templates, y con las referencias a arrays - o sea se juntaron el hambre con
+** las ganas de comer. xD Encima dan por sentado que la función recibe T const,
+** pero ya lo hice con la idea de modificar lo que recibía. :p
+**
+** En fin. Para hacerlo funcionar tanto con mis funciones de prueba como con las
+** de la escuela, he hecho una sobrecarga del template con y sin const.
+*/
+
+template<typename T, typename S>
+void	iter(T * arr_ptr, S const arr_len, void (*fun)(T & a))
 {
-	for (ARR_LEN_T i = 0; i < arr_len; ++i)
-		std::cout << " " << arr_ptr[i];
-	std::cout << std::endl;
-	for (ARR_LEN_T i = 0; i < arr_len; ++i)
-		arr_ptr[i] = fun(arr_ptr[i]);
-	for (ARR_LEN_T i = 0; i < arr_len; ++i)
-		std::cout << " " << arr_ptr[i];
-	std::cout << std::endl;
+	for (S i = 0; i < arr_len; ++i)
+		fun(arr_ptr[i]);
+}
+
+template<typename T, typename S>
+void	iter(T * arr_ptr, S const arr_len, void (*fun)(T const & a))
+{
+	for (S i = 0; i < arr_len; ++i)
+		fun(arr_ptr[i]);
 }
 
 template<typename T>
-T	funky_func(T a)
+void	funky_func(T & a)
 {
-	return  (++a);
+	a += 1;
 }
 
 template<typename T>
-T	halve(T a)
+void	halve(T & a)
 {
-	return (a / 2);
+	a /= 2;
 }
 
 #endif
--- mod07/ex01/main.cpp
+++ mod07/ex01/main.cpp
@@ -6,33 +6,96 @@
 /*   By: mrosario <mrosario@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/23 23:09:45 by mrosario          #+#    #+#             */
-/*   Updated: 2021/10/24 00:08:08 by mrosario         ###   ########.fr       */
+/*   Updated: 2021/10/24 21:25:37 by mrosario         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include "iter.hpp"
 
+#define OUT std::cout
+#define NL std::endl
+
+class Awesome
+{
+	public:
+		Awesome( void ) : _n( 42 ) { return; }
+		int get( void ) const { return this->_n; }
+	private:
+		int _n;
+};
+std::ostream & operator<<( std::ostream & o, Awesome const & rhs ) { o << rhs.get(); return o; }
+template< typename T >
+void print( T const & x ) { std::cout << x << std::endl; return; }
+
 int	main(void)
 {
 	char	chr_array[] = {"abcdefghijklmn"};
+	size_t	chr_array_size = std::strlen(chr_array);
 	int		int_array[2] = {4,2};
 	float	float_array[2] = {4.42, 100.1};
 	
-	std::cout << "FUNKY FUNC" << std::endl;
+	OUT << "FUNKY FUNC" << NL;
+	for (size_t i = 0; i < chr_array_size; ++i)
+		OUT << chr_array[i];
+	OUT << NL;
 	iter(chr_array, std::strlen(chr_array), funky_func<char>);
-	std::cout << std::endl;
-	iter(int_array, 2, funky_func<int>);
-	std::cout << std::endl;
-	iter(float_array, 2, funky_func<float>);
-	std::cout << std::endl;
+	for (size_t i = 0; i < chr_array_size; ++i)
+		OUT << chr_array[i];
+	OUT << NL
 
-	std::cout << "HALVE" << std::endl;
+	<< NL;
+	for (size_t i = 0; i < 2; ++i)
+		OUT << int_array[i];
+	OUT << NL;
+	iter(int_array, 2, funky_func<int>);
+	for (size_t i = 0; i < 2; ++i)
+		OUT << int_array[i];
+	OUT << NL
+
+	<< NL;
+	for (size_t i = 0; i < 2; ++i)
+		OUT << float_array[i];
+	OUT << NL;
+	iter(float_array, 2, funky_func<float>);
+		for (size_t i = 0; i < 2; ++i)
+		OUT << float_array[i];
+	OUT << NL
+
+	<< NL << "HALVE" << NL;
+	for (size_t i = 0; i < chr_array_size; ++i)
+		OUT << chr_array[i];
+	OUT << NL;
 	iter(chr_array, std::strlen(chr_array), halve<char>);
-	std::cout << std::endl;
+	for (size_t i = 0; i < chr_array_size; ++i)
+		OUT << chr_array[i];
+	OUT << NL
+
+	<< NL;
+	for (size_t i = 0; i < 2; ++i)
+		OUT << int_array[i];
+	OUT << NL;
 	iter(int_array, 2, halve<int>);
-	std::cout << std::endl;
+	for (size_t i = 0; i < 2; ++i)
+		OUT << int_array[i];
+	OUT << NL
+
+	<< NL;
+	for (size_t i = 0; i < 2; ++i)
+		OUT << float_array[i];
+	OUT << NL;
 	iter(float_array, 2, halve<float>);
+	for (size_t i = 0; i < 2; ++i)
+		OUT << float_array[i];
+	OUT << NL
+
+	<< NL << "SCHOOL TEST " << NL;
+	int tab[] = { 0, 1, 2, 3, 4 }; // <--- Qué sentido tiene no poder escribir int[] tab. No sería más simple??
+	Awesome tab2[5];
+
+	iter( tab, 5, print );
+	iter( tab2, 5, print );
+
 
 	return (0);
 }
